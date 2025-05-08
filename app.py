2ab9396c23bebfe68afbc1328285b3ed8c008bd1
Sk8alex <excellentforme@gmail.com>
2025-05-08 20:47:56 +0300

    Add Qoutes

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+import random
 
 app = Flask(__name__)
 app.json.ensure_ascii = False
@@ -7,6 +8,52 @@ app.json.ensure_ascii = False
 def hello_world(): # Это функция-обработчик, которая будет вызвана приложением для обработки urlа
     #return "Hello, World!"
     return jsonify(hello="Hello, Studens!!!"), 200
+
+
+quotes = [
+    {
+        "id": 1,
+        "author": "Rick Cook",
+        "text": "Программирование сегодня — это гонка разработчиков программ, стремящихся писать программы с большей и лучшей идиотоустойчивостью, и вселенной, которая пытается создать больше отборных идиотов. Пока вселенная побеждает."
+    },
+    {
+        "id": 2,
+        "author": "Waldi Ravens",
+        "text": "Программирование на С похоже на быстрые танцы на только что отполированном полу людей с острыми бритвами в руках."
+    },
+    {
+        "id": 3,
+        "author": "Mosher’s Law of Software Engineering",
+        "text": "Не волнуйтесь, если что-то не работает. Если бы всё работало, вас бы уволили."
+    },
+    {
+        "id": 4,
+        "author": "Yoggi Berra",
+        "text": "В теории, теория и практика неразделимы. На практике это не так."
+    },
+]
+
+@app.get("/quotes")
+def get_quotes():
+    return jsonify(quotes)
+
+@app.route('/quotes/<int:quote_id>', methods=['GET'])
+def get_quote(quote_id):
+    for quote in quotes:
+        if quote['id'] == quote_id:
+            return jsonify(quote)
+    return f"Quote with id={id} not found", 404
+
+
+@app.route('/quotes/count', methods=['GET'])
+def get_quotes_count():
+    count = len(quotes)
+    return jsonify({"count": count})
+
+@app.route('/quotes/random', methods=['GET'])
+def get_random_quote():
+    quote = random.choice(quotes)
+    return jsonify(quote)
 
 
 about_me = {
